50f8273908783a17ed08a09318600a376e8758bc
MichaLademann <mic.lademann81@gmail.com>
2025-11-17 15:51:42 +0100

    dodano podstawowy interfejs (Start/Stop), timer z prostą logiką i strukturę aplikacji

--- DesktopAppProject/Form1.Designer.cs
+++ DesktopAppProject/Form1.Designer.cs
@@ -2,15 +2,12 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private Button buttonStart;
+        private Button buttonStop;
+        private Label labelStatus;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +17,50 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(20, 20);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 35);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(160, 20);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(120, 35);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Location = new System.Drawing.Point(20, 80);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(260, 25);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "Status: ---";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(320, 150);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "DesktopApp";
+            this.ResumeLayout(false);
         }
-
-        #endregion
     }
 }
--- DesktopAppProject/Form1.cs
+++ DesktopAppProject/Form1.cs
@@ -1,10 +1,37 @@
+using System;
+using System.Windows.Forms;
+
 namespace DesktopAppProject
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Inicjalizacja timera (ograniczona funkcjonalność)
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 2000;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            labelStatus.Text = "Wykonano prostą akcję (timer).";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            labelStatus.Text = "Timer uruchomiony...";
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            labelStatus.Text = "Timer zatrzymany.";
         }
     }
 }
